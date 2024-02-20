66c60d760e35c5c72005a8de0b9b8217403deb83
Валерия Гетьман <valerygetman@gmail.com>
2024-02-20 17:20:44 +0300

    добавление консольного приложения и библиотеки классов вариант 1

---
+++ лаб2/clab_v1/clab_v1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clab_v1
+{
+    public class Class1
+    {
+    }
+}
