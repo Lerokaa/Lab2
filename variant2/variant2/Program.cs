7af6d47cbad8abc625379444917035e4f5077d4b
Arhenon <52978418+Arhenon@users.noreply.github.com>
2024-02-23 21:05:57 +0300

    Вариант 2

---
+++ variant2/variant2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using variant2_lib;
+
+namespace variant2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ variant2/variant2_lib/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant2_lib
+{
+    /// <summary>
+    /// Класс аудитория, принимающий следующие значения: название, ссылка на ответственного сотрудника, количество посадочных мест,количество окон, список ссылок на Оборудование
+    /// </summary>
+    public class Auditorium
+    {
+        private string name;
+        private string responsibleEmployee;
+        private int seatingCapacity;
+        private int windowCount;
+        private List<string> equipmentList;
+        private DateTime creationDate;
+
+
+        //Конструктор класса
+        public Auditorium(string name, string responsibleEmployee, int seatingCapacity, int windowCount, List<string> equipmentList, DateTime? creationDate = null)
+        {
+            this.name = name;
+            this.responsibleEmployee = responsibleEmployee;
+            this.seatingCapacity = seatingCapacity;
+            this.windowCount = windowCount;
+            this.equipmentList = equipmentList;
+            this.creationDate = creationDate ?? DateTime.Now;
+        }
+
+
+        // Метод для добавления оборудования в список
+        public void AddEquipment(string equipment)
+        {
+            equipmentList.Add(equipment);
+        }
+        //https://docs.google.com/document/d/10XwtkjNB6-lRehWP_7qbgWC1nn_oIpa28ezyPXX3B28/edit
+
+    }
+}
