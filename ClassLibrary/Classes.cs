12ee297378545626063b5b796810e5c51a6d123a
Валерия Гетьман <valerygetman@gmail.com>
2024-03-06 19:45:01 +0300

    class Lesson

--- ClassLibrary/Classes.cs
+++ ClassLibrary/Classes.cs
@@ -6,6 +6,30 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+    public class Lesson
+    {
+        private DateTime date;
+        private string disciplina;
+        private string employeer;
+        private string classroom;
+        private string group;
+        private string lesson;
+        private string lessonType;
+
+        public Lesson(DateTime Date, string Disciplina, string Employeer,
+                  string Classroom, string Group, string Lesson,
+                  string LessonType)
+        {
+            date = Date != default ? Date : DateTime.Now;
+            disciplina = Disciplina;
+            employeer = Employeer;
+            classroom = Classroom;
+            group = Group;
+            lesson = Lesson;
+            lessonType = LessonType;
+        }
+    }
+    
     public class Classes
     {
        
