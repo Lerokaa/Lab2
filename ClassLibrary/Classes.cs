ecaa3e33fb2f5a575cf4d8ef9e62fc3d7c2c3863
StAruYViTal9 <148614613+StAruYViTal9@users.noreply.github.com>
2024-03-23 11:28:06 +0300

    var5.cs

--- ClassLibrary/Classes.cs
+++ ClassLibrary/Classes.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+    public class Lesson
+    {
+        private string Surname;
+        private string Name;
+        private string Middlename;
+        private string group;
+        private DateTime DR;
+
+        public Lesson(string Surname, string name, string Middlename, string group, DateTime DR)
+        {
+            date = Date != default ? Date : DateTime.Now;
+            name = Name;
+            surname = Surname;
+            middlename = Middlename;
+            group = Group;
+        }
+    }
+    
     public class Classes
     {
        
