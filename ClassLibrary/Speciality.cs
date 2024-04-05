af065feb24cc932948bf2fb3d0018d15cc33f236
Denis Egerew <egereffden@gmail.com>
2024-04-05 20:11:52 +0300

    Class Speciality

---
+++ ClassLibrary/Speciality.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //https://docs.google.com/document/d/1pDw7bbBJDmeqT8VVwp68BSOF_4yd9cNb9xpH_q2KW0E/edit
+    internal class Speciality
+    {
+        private string Name;
+        private string AbbreviationName;
+
+        public Speciality(string Name, string AbbreviationName)
+        {
+            this.Name = Name;
+            this.AbbreviationName = AbbreviationName;
+        }
+        public Speciality(){}
+    }
+}
