7419d10f44184f67820843b19f74ea3fab0fde80
Dimasimagin <152683305+Dimasimagin@users.noreply.github.com>
2024-04-05 12:13:32 +0300

    v1

--- ClassLibrary/Organization.cs
+++ ClassLibrary/Organization.cs
@@ -8,5 +8,32 @@ namespace ClassLibrary
 {
     internal class Organization
     {
+        private string name;
+        private string uraddress;
+        private string factaddress;
+        private Employee supervisor;
+        public Organization(string name, string uraddress, string factaddress, Employee supervisor)
+        {
+            this.name = name;
+            this.uraddress = uraddress;
+            this.factaddress = factaddress;
+            this.supervisor = supervisor;
+        }
+        public string Name
+        {
+            get { return name; } set { name = value; }
+        }
+        public string Uraddress
+        {
+            get { return uraddress; } set { uraddress = value; }
+        }
+        public Employee Supervisor
+        {
+            get { return supervisor; } set { supervisor = value; }
+        }
+        public string FactAddress
+        {
+            get { return factaddress; } set {  factaddress = value; }
+        }
     }
 }
