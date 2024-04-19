d82bd92504afa99fdf1f4ed4b0ad66616d55605e
xKernelPx <mr.shpak.as@gmail.com>
2024-04-20 01:07:00 +0300

    Changes to be committed: 	modified:   ClassLibrary/Lesson.cs

--- ClassLibrary/Lesson.cs
+++ ClassLibrary/Lesson.cs
@@ -8,5 +8,18 @@ namespace ClassLibrary
 {
     internal class Lesson
     {
+		private DateTime _timeParaStart;
+		private DateTime _timeParaEnd;
+		private DateTime _timeRestStart;
+		private DateTime _timeRestEnd;
+		private Smena _smena;
+		public (DateTime timeParaStart, DateTime timeParaEnd, DateTime timeRestStart, DateTime timeRestEnd, Smena Smena)
+		{
+		    _timeParaStart = timeParaStart;
+		    _timeParaEnd = timeParaEnd;
+		    _timeRestStart = timeRestStart;
+		    _timeRestEnd = timeRestEnd;
+		    _smena = smena;
+		}
     }
 }
