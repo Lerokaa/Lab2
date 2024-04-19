c1543f18d787c1e52f99bb35204067e7cf44987d
ribtie <148614478+ribtie@users.noreply.github.com>
2024-04-19 22:54:17 +0300

    Division.cs

--- ClassLibrary/Division.cs
+++ ClassLibrary/Division.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class Division
+    class Division
+{
+    private string name;
+    private Employee head;
+    private Organization organization;
+    
+    public Department(string name, Employee head, Organization organization)
     {
+        this.name = name;
+        this.head = head;
+        this.organization = organization;
     }
+}
+
+class Employee
+{
+    public string Name { get { return name; } set { name = value; } }
+    public DateTime HireDate { get { return HireDate; } set { HireDate = value; }}
+
+    public Employee(string name)
+    {
+        this.Name = name;
+        this.HireDate = DateTime.Now;
+    }
+}
+
+class Organization
+{
+    public string Name { get { return name; } set { name = value; } }
+
+    public Organization(string name)
+    {
+        this.Name = name;
+    }
+}
 }
