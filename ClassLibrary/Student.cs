f5e6dba9fe955f71bc18dbde6dcadf45c7efb0e2
StAruYViTal9 <148614613+StAruYViTal9@users.noreply.github.com>
2024-04-20 10:48:12 +0300

    Update and rename Classes.cs to Student.cs

--- ClassLibrary/Student.cs
+++ ClassLibrary/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Lesson
+    public class Student
     {
         private string lastName;
         private string firstName;
