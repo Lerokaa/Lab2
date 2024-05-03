b71232e21b6051b0d029b14f5fc42a4f79a53124
ribtie <148614478+ribtie@users.noreply.github.com>
2024-05-03 23:32:10 +0300

    Division.cs

--- ClassLibrary/Division.cs
+++ ClassLibrary/Division.cs
@@ -12,33 +12,15 @@ namespace ClassLibrary
     private Employee head;
     private Organization organization;
     
-    public Department(string name, Employee head, Organization organization)
+    public string Department(string name, Employee head, Organization organization)
     {
         this.name = name;
         this.head = head;
         this.organization = organization;
     }
+      
 }
 
-class Employee
-{
-    public string Name { get { return name; } set { name = value; } }
-    public DateTime HireDate { get { return HireDate; } set { HireDate = value; }}
 
-    public Employee(string name)
-    {
-        this.Name = name;
-        this.HireDate = DateTime.Now;
-    }
-}
-
-class Organization
-{
-    public string Name { get { return name; } set { name = value; } }
-
-    public Organization(string name)
-    {
-        this.Name = name;
-    }
-}
+    
 }
