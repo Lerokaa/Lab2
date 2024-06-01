147ac091fec5fb7ddb1c844f011a79a8d4aa1fd4
Somettir <148618753+Somettir@users.noreply.github.com>
2024-06-01 11:25:56 +0300

    CreateClassroom

--- ConsoleApp/Creator.cs
+++ ConsoleApp/Creator.cs
@@ -73,15 +73,56 @@ namespace ConsoleApp
             return null;
         }
 
-        private static Classroom CreateClassroom()
-        {
-            return null;
-        }
+        public static Classroom CreateClassroom()
+{
+Console.WriteLine("Введите название аудитории: ");
+string name = Console.ReadLine();
 
-        private static Employee CreateEmployee()
+        Classroom classroom = DB.Classrooms.FirstOrDefault<Classroom>(cl => cl.Name == name);
+        if(classroom == null) 
         {
-            return null;
+            Console.WriteLine("Введите количество мест?");
+            int seatingCapacity;
+            while (!int.TryParse(Console.ReadLine(), out seatingCapacity) || seatingCapacity < 0)
+                Console.WriteLine("Нужно ввести целое число =>0");
+            Console.WriteLine("Введите количество окон?");
+            int windowCount;
+            while (!int.TryParse(Console.ReadLine(), out windowCount) || windowCount < 0)
+                Console.WriteLine("Нужно ввести целое число =>0");
+            Console.WriteLine("Введите количество оборудования:");
+            int equipmentCount;
+            while (!int.TryParse(Console.ReadLine(), out equipmentCount) || equipmentCount < 0)
+                Console.WriteLine("Нужно ввести целое число =>0");
+            List<Equipment> equipmentList = new List<Equipment>();
+            for (int i = 0; i < equipmentCount; i++)
+            {
+                Console.WriteLine("Создайте оборудование:");
+                equipmentList.Add(CreateEquipment());
+            }
+            Console.WriteLine("Введите ответственного сотрудника: ");
+            Employee employee = CreateEmployee();
+
+            classroom = new Classroom(name, employee, seatingCapacity, windowCount);
+            foreach (Equipment equipment in equipmentList)
+            {
+                classroom.Equipments.Add(equipment);
+            }
+            DB.Classrooms.Add(classroom);
+            
         }
+        return classroom;
+
+    }
+
+    public static Equipment CreateEquipment()
+    {
+        return null;
+    }
+    public static Employee CreateEmployee()
+    {
+        return null;
+    }
+
 
         public static Discipline CreateDiscipline()
         {
